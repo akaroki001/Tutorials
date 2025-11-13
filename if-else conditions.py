b3b057d94f8830d369a2a012e9a233f467bde66b
Austin K <gkaroki@gmail.com>
2025-11-13 12:10:53 +0300

    Add files via upload

---
+++ if-else conditions.py
@@ -0,0 +1,19 @@
+x = input("indicate the duration you will pay rent in months")
+if x == '12':
+    print("Total rent :4000 per Month ")
+    Total = 4000 * 12
+    print(f'Total rent for year :{Total:}')
+elif x == '6':
+    print("Total rent :5000 per month ")
+    Total = 5000 * 12
+    print(f'Total rent for the year :{Total:}')
+elif x == '3':
+    print("Total rent :6000 per month")
+    Total = 6000 * 3
+    print(f'Total rent for the year :{Total:}')
+elif x == '1':
+    print("Total rent :7000 per month")
+    Total = 7000 * 12
+    print(f'Total rent for the year :{Total:}')
+
+print("welcome to ark apartment")
